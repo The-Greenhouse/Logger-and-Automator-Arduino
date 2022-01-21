75ca607eaee73152218ead6dc001e551100fc117
Vineet Mahajan <vineetmahajan2000@gmail.com>
2022-01-22 03:21:12 +0530

    time module

---
+++ Components/RtcDS1302.ino
@@ -0,0 +1,39 @@
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
+
+
+ThreeWire myWire(4,5,2); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+void setup () 
+{
+    Serial.begin(9600);
+    
+    Rtc.Begin();
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+String get_time(const RtcDateTime& dt)
+{
+    char datestring[20];
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u:%02u"),
+            dt.Hour(),
+            dt.Minute() );
+    
+    return String(datestring);
+}
+
+//src  - https://electropeak.com/learn/interfacing-ds1302-real-time-clock-rtc-module-with-arduino/
+
+void loop () 
+{
+    RtcDateTime now = Rtc.GetDateTime();
+    String tm = get_time(now);
+    
+    Serial.println(tm);
+
+
+    delay(5000); // five seconds
+}
